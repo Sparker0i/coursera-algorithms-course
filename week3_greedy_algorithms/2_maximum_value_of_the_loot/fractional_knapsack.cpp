65cf51206c14032aa50357be7db8d27604213390
Aaditya Menon <aaditya@sparker0i.me>
2018-03-23 21:51:36 +0530

    Week 3 Q2 incorrect solution

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -2,36 +2,47 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <functional>
+#define max(a,b) (((a) > (b)) ? (a) : (b))
+#define min(a,b) (((a) < (b)) ? (a) : (b))
 
 using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
 
-vector<double> divide( const std::vector<int>& a, const std::vector<int>& b )
+int getMax(vector<int> w , vector<int> value) 
 {
-    std::vector<double> result ;
-
-    const std::size_t n = std::min(a.size(), b.size()) ;
-    std::transform( std::begin(a), std::begin(a)+n, std::begin(b),
-                    std::back_inserter(result), std::divides<double>{} ) ;
-    return result ;
+  int max = 0;
+  double v = 0;
+  for (int i = 0; i < w.size(); ++i)
+  {
+    if (w[i] > 0 && max(v , value[i]/w[i]) == value[i]/w[i])
+    {
+      max = i;
+      v = value[i]/w[i];
+    }
+  }
+  return max;
 }
 
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
   double value = 0.0;
   
   // write your code here
-  double weight = 0.0;
-  int i = 0;
-  while (weight <= capacity && weights[i] <= capacity - weight)
+  vector<int> A(weights.size() , 0);
+
+  int n = weights.size();
+  for (int i = 0; i < n; ++i)
   {
-    weight += weights[i];
-    value += values[i];
-    ++i;
-  }
-  if (weight < capacity)
-  {
-    double x = capacity - weight;
-    weight += x;
-    value += ((x * values[i]) / weights[i]);
+    if (capacity == 0)
+      return value;
+    int j = getMax(weights , values);
+    int a = min(weights[j] , capacity);
+    value += a * (float(values[j]) / weights[j]);
+    weights[j] -= a;
+    A[j] += a;
+    capacity -= a;
   }
 
   return value;
@@ -40,16 +51,16 @@ double get_optimal_value(int capacity, vector<int> weights, vector<int> values) 
 int main() {
   int n;
   int capacity;
-  std::cin >> n >> capacity;
+  cin >> n >> capacity;
   vector<int> values(n);
   vector<int> weights(n);
   for (int i = 0; i < n; i++) {
-    std::cin >> values[i] >> weights[i];
+    cin >> values[i] >> weights[i];
   }
 
   double optimal_value = get_optimal_value(capacity, weights, values);
 
-  std::cout.precision(10);
-  std::cout << optimal_value << std::endl;
+  cout.precision(4);
+  cout << optimal_value << endl;
   return 0;
 }
