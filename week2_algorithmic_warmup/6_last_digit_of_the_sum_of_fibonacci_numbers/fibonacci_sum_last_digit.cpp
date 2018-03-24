384d4f416bbb907610369e4379713b8b049a5eae
Aaditya Menon <aaditya@sparker0i.me>
2018-03-24 16:05:05 +0530

    Finished Week 2 Q6

--- week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
+++ week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 
 int fibonacci_sum_naive(long long n) {
-    if (n <= 1)
-        return n;
+    n = (n + 2) % 60;
+    int fib[n + 1];
+    fib[0] = 0;
+    fib[1] = 1;
 
-    long long previous = 0;
-    long long current  = 1;
-    long long sum      = 1;
-
-    for (long long i = 0; i < n - 1; ++i) {
-        long long tmp_previous = previous;
-        previous = current;
-        current = tmp_previous + current;
-        sum += current;
+    int ans = 1;
+    for (int i = 2; i <= n; ++i)
+    {
+        fib[i] = (fib[i - 1] % 10 + fib[i - 2] % 10) % 10;
     }
 
-    return sum % 10;
+    if (fib[n] == 0)
+        return 9;
+    return fib[n] % 10 - 1;
 }
 
 int main() {
