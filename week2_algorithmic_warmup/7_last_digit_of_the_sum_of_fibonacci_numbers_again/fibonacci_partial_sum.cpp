a345b117cf285adebfa0150b11b38d03ed1bdca8
Aaditya Menon <aaditya@sparker0i.me>
2018-03-24 17:23:54 +0530

    Finished Week 2 Q7

--- week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
+++ week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 #include <vector>
 using std::vector;
+using std::cout;
 
 long long get_fibonacci_partial_sum_naive(long long from, long long to) {
+    vector<long long> a(60);
     long long sum = 0;
+    a[0] = 0;
+    a[1] = 1;
 
-    long long current = 0;
-    long long next  = 1;
-
-    for (long long i = 0; i <= to; ++i) {
-        if (i >= from) {
-            sum += current;
-        }
-
-        long long new_current = next;
-        next = next + current;
-        current = new_current;
+    for (int i = 2; i < 60; ++i)
+    {
+        a[i] = a[i - 1] + a[i - 2];
     }
+
+    from %= 60;
+    to %= 60;
+
+    if (to < from)
+        to += 60;
+    
+    for (int j = from; j < to + 1; ++j)
+        sum += a[j % 60];
 
     return sum % 10;
 }
