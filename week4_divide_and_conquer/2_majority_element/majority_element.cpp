b70ea3f6fe42ac421b4deec720949b493f6c4a34
Aaditya Menon <aaditya@sparker0i.me>
2018-03-26 12:02:46 +0530

    Week 4 Q2

--- week4_divide_and_conquer/2_majority_element/majority_element.cpp
+++ week4_divide_and_conquer/2_majority_element/majority_element.cpp
@@ -1,13 +1,38 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <stack>
 
 using std::vector;
 
-int get_majority_element(vector<int> &a, int left, int right) {
-  if (left == right) return -1;
-  if (left + 1 == right) return a[left];
-  //write your code here
+int get_majority_element(vector<int> &a, int left , int right) 
+{
+  std::vector<int> g;
+
+  for (int i : a)
+  {
+    if (!g.empty() && g[g.size() - 1] != i)
+      g.pop_back();
+    
+    else if (g.empty() || g[g.size() - 1] == i)
+      g.push_back(i);
+  }
+
+  if (g.empty())
+    return -1;
+
+  int candidate = g[g.size() - 1];
+  g.pop_back();
+  int count = 0;
+
+  for (int x : a)
+  {
+    if (x == candidate)
+      count += 1;
+  }
+  if (count > a.size() / 2)
+    return candidate;
+
   return -1;
 }
 
@@ -15,8 +40,9 @@ int main() {
   int n;
   std::cin >> n;
   vector<int> a(n);
-  for (size_t i = 0; i < a.size(); ++i) {
+  for (size_t i = 0; i < a.size(); ++i) 
+  {
     std::cin >> a[i];
   }
-  std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+  std::cout << (get_majority_element(a, 0 , a.size()) != -1) << '\n';
 }
