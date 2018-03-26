4278f9613c2371ade109daef10ae784b6f73b194
Aaditya Menon <aaditya@sparker0i.me>
2018-03-26 17:05:14 +0530

    Finished Week 4 Q3

--- week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
+++ week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
@@ -5,30 +5,60 @@
 using std::vector;
 using std::swap;
 
-int partition2(vector<int> &a, int l, int r) {
-  int x = a[l];
-  int j = l;
-  for (int i = l + 1; i <= r; i++) {
-    if (a[i] <= x) {
-      j++;
-      swap(a[i], a[j]);
+int partition(vector<int> &a, int l, int r , int &i, int &j) {
+  i = l - 1;
+  j = r;
+  int p = l - 1 , q = r;
+  int v = a[r];
+
+  while (true)
+  {
+    while (a[++i] < v);
+
+    while (v < a[--j])
+    {
+      if (j == l)
+        break;
+    }
+
+    if (i >= j)
+      break;
+
+    std::swap(a[i] , a[j]);
+
+    if (a[i] == v)
+    {
+      ++p;
+      std::swap(a[p] , a[i]);
+    }
+
+    if (a[j] == v)
+    {
+      --q;
+      std::swap(a[j] , a[q]);
     }
   }
-  swap(a[l], a[j]);
-  return j;
+
+  std::swap(a[i] , a[r]);
+
+  j = i - 1;
+  for (int k = l; k < p; ++k , --j)
+    std::swap(a[k] , a[j]);
+
+  i++;
+  for (int k = r - 1; k > q; --k , ++i)
+    std::swap(a[i] , a[k]);
 }
 
-void randomized_quick_sort(vector<int> &a, int l, int r) {
-  if (l >= r) {
+void quick_sort(vector<int> &a, int l, int r) {
+  if (l >= r)
     return;
-  }
 
-  int k = l + rand() % (r - l + 1);
-  swap(a[l], a[k]);
-  int m = partition2(a, l, r);
+  int i , j;
+  partition(a , l , r , i , j);
 
-  randomized_quick_sort(a, l, m - 1);
-  randomized_quick_sort(a, m + 1, r);
+  quick_sort(a, l, j);
+  quick_sort(a, i, r);
 }
 
 int main() {
@@ -38,7 +68,7 @@ int main() {
   for (size_t i = 0; i < a.size(); ++i) {
     std::cin >> a[i];
   }
-  randomized_quick_sort(a, 0, a.size() - 1);
+  quick_sort(a, 0, a.size() - 1);
   for (size_t i = 0; i < a.size(); ++i) {
     std::cout << a[i] << ' ';
   }
