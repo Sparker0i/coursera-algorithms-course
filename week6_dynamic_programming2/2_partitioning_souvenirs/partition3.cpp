1e84a2922e901e3e0a03ea758accc05d97fe2503
Aaditya Menon <aaditya@sparker0i.me>
2018-04-05 10:29:35 +0530

    Week 6 Q2

--- week6_dynamic_programming2/2_partitioning_souvenirs/partition3.cpp
+++ week6_dynamic_programming2/2_partitioning_souvenirs/partition3.cpp
@@ -1,19 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <numeric>
 
 using std::vector;
 
 int partition3(vector<int> &A) {
-  //write your code here
-  return 0;
+    //write your code here
+    int sum = std::accumulate(A.begin() , A.end() , 0);
+    if (A.size() < 3 || !(sum % 3))
+        return 0;
+    
+    int part = std::floor(sum / 3);
+    vector<vector<int> > table (A.size() + 1 , vector<int>(part + 1 , 0));
+
+    for (int i = 1; i <= part; ++i)
+    {
+        for (int j = 1; j <= A.size(); ++j)
+        {
+            int k = i - A.at(j - 1);
+            if (A.at(j - 1) == i || (k > 0 && table.at(k).at(j - 1) > 0))
+                if (table.at(i).at(j - 1) == 0)
+                    table.at(i).at(j) = 1;
+                else
+                    table.at(i).at(j) = 2;
+            else
+                table.at(i).at(j) = table.at(i).at(j - 1);
+        }
+    }
+
+    if (table.at(part).at(A.size()) == 2)
+        return true;
+    else return false;
 }
 
-int main() {
-  int n;
-  std::cin >> n;
-  vector<int> A(n);
-  for (size_t i = 0; i < A.size(); ++i) {
-    std::cin >> A[i];
-  }
-  std::cout << partition3(A) << '\n';
+int main() 
+{
+    int n;
+    std::cin >> n;
+    vector<int> A(n);
+    for (size_t i = 0; i < A.size(); ++i) {
+        std::cin >> A[i];
+    }
+    std::cout << partition3(A) << '\n';
 }
--- week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
+++ week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
@@ -1,20 +1,24 @@
-# Uses python3
-import sys
-import itertools
+#python3
+def solution(values):
+    total = sum(values)
+    if len(values) < 3 or total % 3:
+        return 0
+    third = total // 3
+    table = [[0] * (len(values) + 1) for _ in range(third + 1)]
 
-def partition3(A):
-    for c in itertools.product(range(3), repeat=len(A)):
-        sums = [None] * 3
-        for i in range(3):
-            sums[i] = sum(A[k] for k in range(len(A)) if c[k] == i)
+    for i in range(1, third + 1):
+        for j in range(1, len(values) + 1):
+            ii = i - values[j - 1]
+            if values[j - 1] == i or (ii > 0 and table[ii][j - 1]):
+                table[i][j] = 1 if table[i][j - 1] == 0 else 2
+            else:
+                table[i][j] = table[i][j - 1]
 
-        if sums[0] == sums[1] and sums[1] == sums[2]:
-            return 1
+    return 1 if table[-1][-1] == 2 else 0
 
-    return 0
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *A = list(map(int, input.split()))
-    print(partition3(A))
+    input()
 
+    items = [x for x in map(int, input().split())]
+    print(solution(items))
